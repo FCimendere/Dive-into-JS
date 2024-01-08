2ade42820b82024d5351db416914b6fd0207b114
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-01-08 21:07:05 +0100

    update tipApp.js

--- tipCalculator/tipApp.js
+++ tipCalculator/tipApp.js
@@ -1,13 +1,13 @@
 function calculateTip() {
     // Get input values
-    var textBill = parseFloat(document.getElementById("textBill").value);
-    var textTip = parseFloat(document.getElementById("textTip").value);
-    var textPerson = parseInt(document.getElementById("textPerson").value);
+    let textBill = parseFloat(document.getElementById("textBill").value);
+    let textTip = parseFloat(document.getElementById("textTip").value);
+    let textPerson = parseInt(document.getElementById("textPerson").value);
   
     // Perform the calculation
-    var tipAsNum = 1 + textTip / 100;
-    var calcPayment = (textBill / textPerson) * tipAsNum;
-    var finalAmount = calcPayment.toFixed(2);
+    let tipAsNum = 1 + textTip / 100;
+    let calcPayment = (textBill / textPerson) * tipAsNum;
+    let finalAmount = calcPayment.toFixed(2);
   
     // Display the result
     document.getElementById("textTotal").innerHTML = "Final Amount: €" + finalAmount;
